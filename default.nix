9cecfbeecfb07b646da343fa6c359f8fc639a349
notquiteamonad <44178347+notquiteamonad@users.noreply.github.com>
2021-03-24 16:52:34 +0000

    Add a haskell.nix build script

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ haskellNix ? import
+    (builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/72beef11fc6ec32a98f1dd0d4dcd072c89595b43.tar.gz")
+    { }
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2009
+, nixpkgsArgs ? haskellNix.nixpkgsArgs
+, pkgs ? import nixpkgsSrc nixpkgsArgs
+}: pkgs.haskell-nix.project {
+  src = pkgs.haskell-nix.haskellLib.cleanGit {
+    name = "tie";
+    src = ./.;
+  };
+}
