6c8170673b380e4a8820372291c084d1e0f80594
notquiteamonad <44178347+notquiteamonad@users.noreply.github.com>
2021-03-24 22:15:21 +0000

    Add release/linux.nix for static linux builds with musl

---
+++ release/linux.nix
@@ -0,0 +1,14 @@
+let
+  tie = import ../default.nix { };
+  pkgs = import <nixpkgs> { };
+in
+tie.projectCross.musl64.hsPkgs.tie.components.exes.tie-exe.override {
+  configureFlags = [
+    "--disable-executable-dynamic"
+    "--disable-shared"
+    "--ghc-option=-optl=-pthread"
+    "--ghc-option=-optl=-static"
+    "--ghc-option=-optl=-L${pkgs.gmp6.override { withStatic = true; }}/lib"
+    "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
+  ];
+}
