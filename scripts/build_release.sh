e1a6871c6a191809c076381d5294ab2a90d6319b
notquiteamonad <44178347+notquiteamonad@users.noreply.github.com>
2021-03-26 23:48:38 +0000

    Update build_release script to archive windows build automatically

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env sh
 
+set -e
+
 projectDir=$(git rev-parse --show-toplevel)
 
-rm -rf $projectDir/build
+sudo rm -rf $projectDir/build
 mkdir -p $projectDir/build/linux
 
 # Linux Build
@@ -11,7 +13,11 @@ cp -L ./result/bin/tie-linux $projectDir/build/linux/tie
 
 # Windows Build
 nix-build $projectDir/nix/release/windows.nix
-cp -LR ./result/bin $projectDir/build/windows
+cp -LR ./result/bin $projectDir/build/tie-windows
 
 rm result
 
+oldPwd=$(pwd)
+cd $projectDir/build
+tar cvzf tie-windows.tar.gz tie-windows
+cd $oldPwd
